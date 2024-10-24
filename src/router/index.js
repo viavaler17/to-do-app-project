26cbd3462305ee272bcf91a253f16c621cf547db
stickymittens <karolinapalade999@gmail.com>
2024-10-24 19:29:46 +0200

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,35 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
 
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'home',
-//       component: HomeView
-//     },
-//     {
-//       path: '/about',
-//       name: 'about',
-//       // route level code-splitting
-//       // this generates a separate chunk (About.[hash].js) for this route
-//       // which is lazy-loaded when the route is visited.
-//       component: () => import('../views/AboutView.vue')
-//     }
-//   ]
-// })
+import Home from '@/views/Home.vue';
+import BrowseRecipes from '@/views/BrowseRecipes.vue';
+import About from '@/views/About.vue';
+import Contact from '@/views/Contact.vue';
+import Login from '@/views/Login.vue';
+import SignUp from '@/views/SignUp.vue';
+import ProfileMain from '@/views/LoggedIn/ProfileMain.vue';
+import Favourited from '@/views/LoggedIn/Favourited.vue';
+import ShoppingList from '@/views/LoggedIn/ShoppingList.vue';
+import ProfileDetails from '@/views/LoggedIn/ProfileDetails.vue';
+import NotFound from '@/views/NotFound.vue';
 
-// export default router
+
+const routes = [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/browserecipes', name: 'Browse Recipes', component: BrowseRecipes },
+    { path: '/about', name: 'About', component: About},
+    { path: '/contact', name: 'Contact', component: Contact },
+    { path: '/login', name: 'Log in', component: Login },
+    { path: '/signup', name: 'Sign up', component: SignUp },
+    { path: '/profile', name: 'Profile', component: ProfileMain},
+    { path: '/favourited', name: 'Favourited recipes', component: Favourited },
+    { path: '/shippinglist', name: 'Shopping List', component: ShoppingList },
+    { path: '/profiledetails', name: 'Profile details', component: ProfileDetails },
+    { path: '/:pathMatch(.*)*', name: 'Not Found', component: NotFound }
+  ];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+  });
+
+export default router
