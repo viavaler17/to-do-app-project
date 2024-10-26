e8b743c23a43bad2527c8f5a76fb8bf2b9ba863a
Valeria Iatchiv <viavaler@amazon.es>
2024-10-26 10:51:42 +0200

    Table with recipes in BrowseRecipes

--- src/views/BrowseRecipes.vue
+++ src/views/BrowseRecipes.vue
@@ -1,12 +1,14 @@
 <script setup>
 import SearchComponent from '@/components/SearchComponent.vue';
 import CategoriesTags from '@/components/CategoriesTags.vue';
+import FoodDatabaseComponent from '@/components/FoodDatabaseComponent.vue';
 
 </script>
 
 <template>
     <SearchComponent/>
     <CategoriesTags/>
+    <FoodDatabaseComponent></FoodDatabaseComponent>
 </template>
 
 <style scoped>
