aa6f76dddd4981b6ca56eb4afbc176678d530991
Valeria Iatchiv <viavaler@amazon.es>
2024-11-09 10:07:49 +0100

    supabase vite

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_BASE_URL;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_API_KEY;
   
 export const supabase = createClient(supabaseUrl, supabaseKey);
